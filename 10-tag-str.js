0d58b3282ced5d8559d4fd148cfb819f24bc4510
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-01-20 12:00:16 +0530

    10 - tag

---
+++ 10-tag-str.js
@@ -0,0 +1,20 @@
+function html(format, user, text) {
+  console.log(format, user, text);
+  let sanitizedText = text;
+
+  const replaceables = [
+    { before: /&/g, after: '&amp;' },
+    { before: /'/g, after: '&apos;' },
+    { before: /"/g, after: '&quot;' },
+    { before: /</g, after: '&lt;' },
+    { before: />/g, after: '&gt;' },
+  ];
+
+  replaceables.forEach((replaceable) => {
+    sanitizedText = sanitizedText.replace(replaceable.before, replaceable.after);
+  });
+  return format[0] + user + format[1] + sanitizedText + format[2];
+}
+
+module.exports = html;
+// console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
---
+++ 10-tag-str.test.js
@@ -0,0 +1,9 @@
+const html = require('./10-tag-str');
+
+test('Sample string', () => {
+  const user = 'domenic';
+  const text = '<dl> is a fun tag';
+  const expected = '<b>domenic says</b>: \"&lt;dl&gt; is a fun tag\"';
+  const format = html`<b>${user} says</b>: "${text}"`;
+  expect(format).toBe(expected);
+});
