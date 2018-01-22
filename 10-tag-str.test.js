8ba9a3d745d0e8487a004f459af26a2842732c77
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-22 15:40:39 +0530

    10 more test cases

--- 10-tag-str.test.js
+++ 10-tag-str.test.js
@@ -15,3 +15,19 @@ test('Empty string', () => {
   const format = html`${user}${text}`;
   expect(format).toBe(expected);
 });
+
+test('Duplicate string', () => {
+  const user = 'domenic';
+  const text = '<dl> is a fun tag <dl>';
+  const expected = '<b>domenic says</b>: "&lt;dl&gt; is a fun tag &lt;dl&gt;"';
+  const format = html`<b>${user} says</b>: "${text}"`;
+  expect(format).toBe(expected);
+});
+
+test('All instances', () => {
+  const user = 'domenic';
+  const text = '&\'"<>';
+  const expected = '<b>domenic says</b>: "&amp;&apos;&quot;&lt;&gt;"';
+  const format = html`<b>${user} says</b>: "${text}"`;
+  expect(format).toBe(expected);
+});
