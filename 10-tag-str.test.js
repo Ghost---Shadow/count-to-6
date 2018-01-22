160b4165d3bf55a36d540b5d8dc466fc3f8d3527
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-22 14:02:46 +0530

    10 tag test typo fix

--- 10-tag-str.test.js
+++ 10-tag-str.test.js
@@ -3,7 +3,7 @@ const html = require('./10-tag-str');
 test('Sample string', () => {
   const user = 'domenic';
   const text = '<dl> is a fun tag';
-  const expected = '<b>domenic says</b>: \"&lt;dl&gt; is a fun tag\"';
+  const expected = '<b>domenic says</b>: "&lt;dl&gt; is a fun tag"';
   const format = html`<b>${user} says</b>: "${text}"`;
   expect(format).toBe(expected);
 });
