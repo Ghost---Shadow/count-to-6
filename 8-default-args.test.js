6bc3c3012bca97ca27168bfe0407618ef53d06d2
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-22 15:07:19 +0530

    8 describe it

--- 8-default-args.test.js
+++ 8-default-args.test.js
@@ -1,21 +1,23 @@
 const midpoint = require('./8-default-args');
 
-test('Both parameters', () => {
-  expect(midpoint(0.1, 0.3)).toBeCloseTo(0.2);
-});
+describe('Midpoint tests', () => {
+  it('Both parameters', () => {
+    expect(midpoint(0.1, 0.3)).toBeCloseTo(0.2);
+  });
 
-test('Lower missing', () => {
-  expect(midpoint(undefined, 0.3)).toBeCloseTo(0.15);
-});
+  it('Lower missing', () => {
+    expect(midpoint(undefined, 0.3)).toBeCloseTo(0.15);
+  });
 
-test('Upper Missing', () => {
-  expect(midpoint(0.1, undefined)).toBeCloseTo(0.55);
-});
+  it('Upper Missing', () => {
+    expect(midpoint(0.1, undefined)).toBeCloseTo(0.55);
+  });
 
-test('One parameter', () => {
-  expect(midpoint(0.1)).toBeCloseTo(0.55);
-});
+  it('One parameter', () => {
+    expect(midpoint(0.1)).toBeCloseTo(0.55);
+  });
 
-test('Both missing', () => {
-  expect(midpoint()).toBeCloseTo(0.5);
+  it('Both missing', () => {
+    expect(midpoint()).toBeCloseTo(0.5);
+  });
 });
